7a6db94887e150f30670ab095e94cf2151a84352
Amadou Oury Diallo <gaoualmanjallow@gmail.com>
2025-03-21 16:36:25 +0800

    Register models in respective admin.py files for each app

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,3 +1,7 @@
-from django.contrib import admin
+
 
 # Register your models here.
+from django.contrib import admin
+from .models import Booking  # Import the models
+
+admin.site.register(Booking)    # Register Booking model
--- facilities/admin.py
+++ facilities/admin.py
@@ -1,3 +1,7 @@
-from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Facility # Import the models
+
+
+admin.site.register(Facility)   # Register Facility model
